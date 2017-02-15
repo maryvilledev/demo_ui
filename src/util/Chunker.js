07d7ef0ed985a93da45d10768e75f56326d45000
Dane Johnson <dane_johnson@ymail.com>
2017-02-15 10:30:22 -0600

    Updated chunker to work with recursive code

--- src/util/Chunker.js
+++ src/util/Chunker.js
@@ -6,32 +6,73 @@ const startTagRegex = /<!([^/]*?)>/
 const endTagRegex = /<!\s*?\/>/
 
 export default function chunk(code, onMouseEnter) {
-  let chunks = [];  //Holds generated React elements
-  let tagStart = startTagRegex.exec(code); //Find the first tag
-  while (tagStart ) {
-    //While we're still finding tags
-    chunks.push(code.substr(0, tagStart.index)) //Push all the code before the tag
-    let {lang, concept} = getParams(tagStart[1]) //Get the tag params
-    code = code.substr(tagStart.index + tagStart[0].length); //Cut off the tag
-    const endTag = endTagRegex.exec(code) //Find the end tag
-                                        //TODO fix this to work with nested tags
-    const innerCode = code.substr(0, endTag.index); //This is the code between the tags
-    chunks.push(
+  return <span>{chunkInner(code, onMouseEnter)}</span>
+}
+
+function chunkInner(code, onMouseEnter) {
+  let chunks = [];
+  //Enter a loop to iterate over the code
+  let startTag = startTagRegex.exec(code);
+  while(startTag) {
+    //Chop off all before it, so as long as it has length
+    if(startTag.index > 0) {
+      chunks.push(<span>{code.substr(0, startTag.index)}</span>);
+      code = code.substr(startTag.index);
+    }
+
+    //Get position of it's mate
+    let endTag = findMate(code);
+    //Slice out the inner bit
+    let innerCode = code.substring(startTag[0].length, endTag.index);
+    //Wrap it in a code chunk
+    const {concept, lang} = getParams(startTag[1])
+    const chunk = (
       <CodeChunk
-        lang={lang}
         concept={concept}
+        lang={lang}
         color={getColor()}
-        onMouseEnter={onMouseEnter}
-        >{chunk(innerCode, onMouseEnter)}</CodeChunk>)
-    code = code.substr(endTag.index + endTag[0].length)
-    tagStart = startTagRegex.exec(code); //Move to the next start tag
+        onMouseEnter={onMouseEnter}>
+          {chunkInner(innerCode, onMouseEnter)}
+        </CodeChunk>
+    );
+    chunks.push(chunk);
+    //Slice off the inner code and tags
+    code = code.substr(endTag.index + endTag[0].length);
+    //Find the next start tag
+    startTag = startTagRegex.exec(code);
   }
-  chunks.push(code); //Add the tail code
-  return <span>{chunks}</span>;
+  //If there is anything left, wrap it and add it
+  if (code.length > 0) {
+    chunks.push(<span>{code}</span>)
+  }
+  return chunks;
+}
+
+function findMate(code) {
+  const startTagRegexLocal = /<![^/]*?>/g
+  const endTagRegexLocal = /<!\s*?\/>/g
+  //Find first start tag, just ignore it
+  startTagRegexLocal.exec(code);
+  //Find each tag until we've found more close tags than open tags
+  let counter = 0;
+  let nextStartTag = startTagRegexLocal.exec(code)
+  let nextEndTag = endTagRegexLocal.exec(code)
+  while (true) {
+    if (nextStartTag === null || nextEndTag.index < nextStartTag.index) {
+      if(counter <= 0) {
+        return nextEndTag;
+      }
+      counter--;
+      nextEndTag = endTagRegexLocal.exec(code)
+    } else if (nextStartTag.index < nextEndTag.index) {
+      counter++;
+      nextStartTag = startTagRegexLocal.exec(code);
+    }
+  }
 }
 
 function getParams(tag) {
-  const paramsRegex = /(\S*)=(\S*)/g
+  const paramsRegex = /(\S*)=(\S*)/g;
   let obj = {};
   let param;
   while ((param = paramsRegex.exec(tag)) !== null) {
