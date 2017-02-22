92824c85074a8f91ad6aa79d7748131ec214bfe0
Dane Johnson <dane_johnson@ymail.com>
2017-02-22 10:17:11 -0600

    Added dummy code

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,12 @@
       <div class="col-sm-2">
         <filter-box :filters="filters" @toggle="toggleFilter"></filter-box>
       </div>
-      <div class="col-sm-8 tall">
+      <div class="col-sm-6 tall">
         <code-area></code-area>
       </div>
-      <div class="col-sm-2"></div>
+      <div class="col-sm-4">
+        <desc-area :title="title" :description="description" :syntax="syntax"></desc-area>
+      </div>
     </div>
   </div>
 </template>
@@ -17,11 +19,29 @@ import CodeArea from './components/CodeArea'
 import FilterBox from './components/FilterBox'
 import DescArea from './components/DescArea'
 
+const title = `For loop in Java`
+
+const description = `
+The for loop is a control structure that executes a block of code a specified number of times.
+`
+const syntax = `
+for(initialization; booleanExpression; update) {
+    //statements
+} //end for loop
+`
+
+const filters = [
+  {text: "for", value: true},
+  {text: "while", value: false}
+]
 export default {
   name: 'app',
   data() {
     return{
-      filters: [{text: 'for-in', value: false}, {text: 'for-each', value: true}]
+      filters: filters,
+      title: title,
+      description: description,
+      syntax: syntax
     }
   },
   methods: {
